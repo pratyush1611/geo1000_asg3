0a3b21e8e5e0f56ba7b1434fa3f12ffe0b3d3b8c
Pratyush <pratyushkmr13@gmail.com>
2020-09-30 11:04:47 +0200

    firtst commit to make repo

---
+++ hunt.py
@@ -0,0 +1,24 @@
+# GEO1000 - Assignment 3
+# Authors:
+# Studentnumbers:
+
+def read_grid(filenm):
+    """
+    """
+    pass
+
+
+def visit(table, steps_allowed, path):
+    """
+    """
+    pass
+
+
+def hunt(filenm, max_steps):
+    """
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print(hunt('finite.txt', 20))
